c38b15b0497bd60cf76ed81d2a6df85758ff64ec
viviane <v.buschwallace@yahoo.com>
2021-04-28 13:36:48 +0200

    checking file

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,3 +1,4 @@
+// Checking if this is the file
 console.log("------------Password generator--------------");
 // Create a program that generates a random password with the length of 30 characters, every time you call the function, it should give you a different password.
 // Examples:
